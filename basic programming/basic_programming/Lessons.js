98a742557b6c791ecc805d5e2794d4b9d025eed1
nichollecera11 <nichollecera11@gmail.com>
2023-05-15 19:00:33 +0800

    basic_programming

---
+++ basic programming/basic_programming/Lessons.js
@@ -0,0 +1,160 @@
+//My First Javascript Exercises
+
+// //Number
+// var x = 10
+
+// //string
+// var x = '10'
+// var y = 'Tang ina mo'
+// var zz = 'Another tang ina mo'
+
+// //Boolean
+// var x = true;
+
+// //Object
+// var obj = {};
+// var objPerson = {
+//     firstName: 'Nichol',
+//     age: 29,
+//     address: "Zone 8 Patag Cagayan de Oro City",
+//     Gender: 'Male',
+// };
+
+
+// // Dynamic
+// let y = 12;
+// y = true;
+
+// //Concatenation
+// let abc = 'hello'
+// let def = 'yawa man diay ka'
+
+// console.log (abc + '-' + def)
+
+// Boolean sample
+
+// let a = 5;
+// let b = 6;
+// let c = 5
+
+// console.log(a === c);
+
+//Null Sample
+
+// let asd = null
+
+// Symbol sample
+
+// //Sample Type of *uses for checking
+// console.log(typeof b);
+
+
+// let x = "uban"
+// let y = "dili uban"
+// let z = "pero iya abang"
+
+// console.log(x + ' ' + z);
+
+
+//Block scope sample
+
+// var firstNameBasis = 'Nichol';
+// var lastNameBasis = 'Lecera';
+// function sum(x, y) {
+//     // return x + y;
+//     console.log(firstNameBasis);
+// }
+
+//Constant cannot be changed
+//Function Sample *uses for add to cart, admin panel ** do not go outside the curly braces para di mag error
+//be careful how to use var, var is defined for all, constant and let are fixed and in block
+//Let is for specific inside the block only
+//Constant is fixed, need to have value
+
+//Example of update
+// let xxx = 12345;
+    // xxx = 12341235;
+
+// let a = 10;
+// function printData() {
+//     let x = 12;
+//     let y = 13;
+
+//     console.log(x + y);
+
+//     if (true) {
+//         let z = 100;
+
+//         console.log(x + y + z);
+//     }
+//     }
+
+// printData(); // Call
+
+
+// how to declare ideal varriable should be defined with the following to know if its the right one
+// var studentsArray = []
+
+// single liner  var name: ``, age: ``, email: ``
+// pwede rin na ganitong format
+// var name = ``;
+// var age = ``;
+// var address = ``;
+
+// Arithmetic Operation
+
+// function sum(x, y, z)
+// {
+//     return x + y + z;
+// }
+// var sumOutput = sum(1, 2, 3);
+// console.log(sumOutput);
+
+// function difference(x, y, z)
+// {
+//     return x - y - z;
+// }
+// var differenceOutput = difference(5, 3, 1);
+// console.log(differenceOutput);
+
+// function computerAverageBy5(x, y, z)
+// {
+//     return ((x + y + z) / 2) * 5;
+// }
+// var computerAverageBy5Output = computerAverageBy5(5, 3, 1);
+// console.log(computerAverageBy5Output);
+
+//Modulo Operations
+// function checkIfEven(x) {
+//     return x % 2 === 0;
+// }
+// var checkOutput = checkIfEven(5);
+// if (checkOutput) {
+//     console.log('The number is even');
+// }
+// else {
+//     console.log('The number is odd');
+// }
+// console.log(checkOutput);
+
+//LOOPING increment and decrement
+// addition + 1 
+// let count = 5;
+// count++;
+// console.log(count);
+
+// //subtraction - 1 
+// count--;
+// console.log(count);
+
+// // Looping Example
+// for (var i = 0; i < count; i++) {
+//     console.log('i--', i);
+// }
+
+// // Assignment Operator
+// let x = 10, y = 5;
+
+// x += y;
+// //meaning value of (x) x= x+y; the output will be the x
+// console.log(x);
